b321e328c008c4d80f3253d1cd88995dc89dec6e
ruboo23 <rubengarciarodrigo23@gmail.com>
2023-10-02 23:22:29 +0200

    a

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            let iconName: string = "";
+            let colour : string = "black"
+            switch (route.name) {
+              case "Mapa": iconName = "map"
+                break;
+              case "Feeds": iconName = "home"
+                break;
+              case "Buscador": iconName = "search"
+                break;
+              case "Perfil": iconName = "user"
+            }
+            if (!focused) {colour = "grey"}
+            return <Icon name={iconName} size={30} color={colour} />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: { backgroundColor: '#eaeaea' },
+          headerStyle: { backgroundColor: '#8cbcf7' },
+          headerTitleStyle: { fontSize: 30 }
+        })}
+      >
+        <Tab.Screen name='Feeds' component={View} />
+        <Tab.Screen name='Mapa' component={View} />
+        <Tab.Screen name='Buscador' component={View} />
+        <Tab.Screen name='Perfil' component={View} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
