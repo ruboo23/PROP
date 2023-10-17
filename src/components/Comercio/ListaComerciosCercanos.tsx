1e4ad341161316dfe674bcf701fc58936989d42e
Jesús Fiérrez Ruipérez <JFIERUI@upv.edu.es>
2023-10-17 16:20:05 +0200

    Eliminada clase Vista comercio cercano

--- src/components/Comercio/ListaComerciosCercanos.tsx
+++ src/components/Comercio/ListaComerciosCercanos.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView, View } from 'react-native';
-import VistaComercioCercano from './VistaComercioCercano';
 import  {GetComercioByName}  from "../../Servicies/ComercioService";
 import TicketComercioCercano from './TicketComercioCercano';
 import * as Location from 'expo-location'
