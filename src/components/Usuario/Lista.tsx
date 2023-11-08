0d67b44811b4d2b9d9bc128dcab996614cef432c
ruboo23 <rubengarciarodrigo23@gmail.com>
2023-11-08 18:59:02 +0100

    Listas Componentes

---
+++ src/components/Usuario/Lista.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+const ListaPortada = ({Nombre, Index}:any) => {
+  return (
+    <View style={[
+        {
+            borderColor: "grey",
+            borderRadius: 5,
+            borderWidth: 2,
+            width: '49.4%',
+            height: '100%',
+        },
+        Index % 2 === 0
+        ? {
+          marginRight: 2,
+        } : {
+          marginLeft: 2
+        }
+      ]}>
+      <Image
+        source={{ uri: "https://www.civitatis.com/blog/wp-content/uploads/2020/10/centro-valencia.jpg" }}
+        style={styles.image}
+      />
+      <Text style={styles.text}>{Nombre}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: "grey",
+    borderRadius: 5,
+    borderWidth: 2,
+    width: '50%',
+    height: '100%',
+  },
+  image: {
+    height: 150
+  },
+  text: {
+   
+    fontSize: 16,
+    backgroundColor: 'grey'
+  }
+});
+
+export default ListaPortada;
--- src/components/Usuario/UsuarioListas.tsx
+++ src/components/Usuario/UsuarioListas.tsx
@@ -1,16 +1,32 @@
-import { AccessibilityInfo, Button, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { AccessibilityInfo, Button, StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 import { NavigationContainer, useScrollToTop } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
+import ListaPortada from './Lista';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 export default function UsuarioListas() {
+  const [Tienelista, setTieneLista] = useState(true)
+  const [listas, setListas] = useState(["Valencia", "Barcelona", "Madrid", "Comida", "Cena", "Carnicerias", "meriendas", "LÑAKSHD"])
   return (
-    <View style={styles.screenContainer}>
-      <Text>No tienes listas añadidas</Text>
-      <Text style={styles.subtitle}>Añade una ayudandote del boton inferior.</Text>
+    
+    <View style={styles.screenContainerFlatList}>
+     { !Tienelista ?
+     <View style={styles.screenContainerText}>
+     <Text>No tienes listas añadidas</Text>
+     <Text style={styles.subtitle}>Añade una ayudandote del boton inferior.</Text>
+     </View>
+     : 
+      <FlatList 
+        data={listas} 
+        numColumns={2} 
+        renderItem={({item, index}) => <ListaPortada Nombre={item} Index={index}/>}
+        ItemSeparatorComponent={() => <View style={{height: 5, width: 10}} />}
+        />
+     }
       <View style={styles.addButtonContainer}>
-        <TouchableOpacity style={styles.addButton}>
+        <TouchableOpacity style={styles.addButton}
+        >
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
@@ -19,7 +35,13 @@ export default function UsuarioListas() {
 }
 
 const styles = StyleSheet.create({
-  screenContainer: {
+  screenContainerFlatList: {
+    flex: 1,
+    paddingTop: 20, 
+    marginHorizontal: 5,
+    justifyContent: 'center'
+  },
+  screenContainerText: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -29,21 +51,21 @@ const styles = StyleSheet.create({
     color: 'grey',
   },
   addButtonContainer: {
-    position: 'absolute', 
-    bottom: 20, 
-    right: 20, 
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
   },
   addButton: {
     backgroundColor: 'red',
-    width: 50, 
-    height: 50, 
-    borderRadius: 25, 
+    width: 50,
+    height: 50,
+    borderRadius: 25,
     justifyContent: 'center',
     alignItems: 'center',
   },
   buttonText: {
     color: 'white',
     textAlign: 'center',
-    fontSize: 24, 
+    fontSize: 24,
   },
 });
