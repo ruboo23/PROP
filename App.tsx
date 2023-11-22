b55e45fc74ef05912bc5d351beda43082d7442b5
Jesús Fiérrez Ruipérez <JFIERUI@upv.edu.es>
2023-11-22 17:14:19 +0100

    Implantadas notificaciones

--- App.tsx
+++ App.tsx
@@ -17,6 +17,8 @@ const Tab = createBottomTabNavigator();
 
 function App() {
   registerNNPushToken(14728, 'Dq9t3wG5tveDAAoXbbJh8b');
+  registerNNPushToken(14897, '1hw8IfFQUjr3vDsWNC4lNU');
+  registerNNPushToken(14895, 'W3VadISHlovbDiB04l4I4O');
   return (
     <ContextProvider>
     <NavigationContainer>
