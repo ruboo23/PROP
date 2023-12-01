d6f72f740520c0836638a0db6417a2eb548956f3
Andres Felder <andyfelder16@gmail.com>
2023-12-01 12:19:55 +0100

    agrego estilo de mapa y busqueda de comercios

--- src/screens/map/index.tsx
+++ src/screens/map/index.tsx
@@ -12,9 +12,10 @@ import SearchBar from '../../components/searchBar';
 import { mapCoordinates } from '../../mappers/location';
 import { LocationObjectType, useGlobalState } from '../../components/context';
 import { GetAllTipos } from '../../Servicies/TipoComercioService';
-import ValoracionEstrellas from '../../components/Comercio/Reseña/ValoracionEstrellas';
 import { calcularDistancia } from '../../components/Comercio/ListaComerciosCercanos';
 import ModalDropdown from 'react-native-modal-dropdown';
+import ValoracionEstrellas from '../../components/Comercio/Reseña/ValoracionEstrellas';
+import { Searchbar } from 'react-native-paper';
 
 let cancelToken: any;
 let timer: ReturnType<typeof setTimeout>;
@@ -120,6 +121,7 @@ export default function MapScreen() {
     , [searchName]);
 
     const onSearchChange = async (name: string) => {
+      setSearchName(name);
       if (!!timer) {
           clearTimeout(timer)
       }
@@ -148,69 +150,77 @@ export default function MapScreen() {
 
     return (
       <View style={{ flex: 1 }}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10, marginRight: 12 }}>
-          <SearchBar onSearchChange={setSearchName} />
-          <TouchableOpacity onPress={() => setFilterModalVisible(true)}>
-            <Icon name='filter' size={24} color='black' />
-          </TouchableOpacity>
-        </View>
         <View style={styles.loader}>
-        {!mapLoaded && <Text>Cargando mapa...</Text>}
-        {loadingMarkers && <Text>Cargando comercios...</Text>}
-        {location?.latitude && location?.longitude && (
+        {!mapLoaded && loadingMarkers ? 
+        (<View style={{ flex: 1, justifyContent: 'center', marginBottom: '10%', alignItems: 'center'}}>
+          <Image
+            source={require('../../../assets/loading1.gif')}
+            style={{ height: 50, width: 50 }}
+          />
+        </View> )
+        : 
+        location?.latitude && location?.longitude && (
           <SafeAreaView style={{ flex: 1, width: '100%', height: '100%' }}>
             {openList ? 
-            <StoresList markers={markers} /> :
-              <MapView
-              style={styles.map}
-              customMapStyle={mapStyle}
-              initialRegion={{
-                latitude: location.latitude,
-                longitude: location.longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }}
-              showsUserLocation={true}
-              onMapReady={handleMapReady}
-            >
-              {markers.map((marker, index) => (
-                  <Marker
-                      key={index}
-                      coordinate={{ latitude: marker.latitud, longitude: marker.longitud}}
-                      title={marker.nombre}
-                      description={marker.descripcion}
-                      onCalloutPress={(e) => {
-                        navigation.navigate('PerfilComercio', {id: marker.id, esComercioLogueado: false})
-                      }}
+            <>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10, marginRight: 12, zIndex: 100 }}>
+                <TouchableOpacity onPress={() => setFilterModalVisible(true)} style={{ backgroundColor: 'white', borderRadius: 10, padding: 10, borderColor: 'black', borderWidth: 1, marginRight: 12, height: 40, width: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                  <Image source={require('../../../assets/Vector.png')} style={{ width: 16, height: 16 }} />
+                </TouchableOpacity>
+                <Searchbar
+                      onChangeText={onSearchChange}
+                      inputStyle={{ bottom: 9 }}
+                      style={{ backgroundColor: 'white', borderColor: 'black', borderWidth: 1, width: "88%", borderRadius: 10, maxHeight: 40, overflow: 'hidden' }}
                   >
-                    <Callout>
-                        <SafeAreaView style={styles.container}>
-                              <Image source={{uri: `https://propapi-ap58.onrender.com/api/Imagen/${marker.imagenNombre}`}} style={{ width: 22, height: 22}} />
-                          <View style={{ display: 'flex', flexDirection: 'column' }}>
-                            <Text style={{ flex: 1, fontWeight: 'bold' }}>
-                              {marker.nombre}
-                            </Text>
-                            <Text style={{ flex: 1 }}>
-                              {marker.descripcion}
-                            </Text>
-                            <Text style={{ flex: 1, color: '#999' }}>
-                              {marker.direccion}
-                            </Text>
-                          </View>
-                            
-                        </SafeAreaView>
-                    </Callout>
-                  </Marker>
-              ))}
-            </MapView>}
+                </Searchbar>
+              </View>
+              <StoresList markers={markers} types={storeTypes.map((tipo) => tipo.label).slice(1)} isSearching={!!searchName && searchName != ""} />
+            </>
+              :
+            <>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10, marginRight: 12, zIndex: 100 }}>
+                <TouchableOpacity onPress={() => setFilterModalVisible(true)} style={{ backgroundColor: 'white', borderRadius: 10, padding: 10, borderColor: 'black', borderWidth: 1, marginRight: 12, height: 40, width: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                  <Image source={require('../../../assets/Vector.png')} style={{ width: 16, height: 16 }} />
+                </TouchableOpacity>
+                <Searchbar
+                      onChangeText={onSearchChange}
+                      inputStyle={{ bottom: 9 }}
+                      style={{   backgroundColor: 'white', borderColor: 'black', borderWidth: 1, width: "88%", borderRadius: 10, maxHeight: 40, overflow: 'hidden' }}
+                  >
+                </Searchbar>
+              </View>
+                <MapView style={styles.map} customMapStyle={mapStyle} initialRegion={{ latitude: location.latitude, longitude: location.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421, }} showsUserLocation={true} onMapReady={handleMapReady} >
+                {markers.map((marker, index) => (
+                    <Marker key={index} coordinate={{ latitude: marker.latitud, longitude: marker.longitud}} title={marker.nombre} description={marker.descripcion} image={require('../../../assets/Marker.png')} onCalloutPress={(e) => { navigation.navigate('PerfilComercio', {id: marker.id, esComercioLogueado: false}) }} >
+                      <Callout>
+                          <SafeAreaView style={styles.container}>
+                            <Image source={{uri: `https://propapi-ap58.onrender.com/api/Imagen/${marker.imagenNombre}`}} style={{ width: 22, height: 22}} />
+                            <View style={{ display: 'flex', flexDirection: 'column' }}>
+                              <Text style={{ flex: 1, fontWeight: 'bold' }}>
+                                {marker.nombre}
+                              </Text>
+                              <Text style={{ flex: 1 }}>
+                                {marker.descripcion}
+                              </Text>
+                              <Text style={{ flex: 1, color: '#999' }}>
+                                {marker.direccion}
+                              </Text>
+                            </View>   
+                          </SafeAreaView>
+                      </Callout>
+                    </Marker>
+                ))}
+              </MapView>
+            </>}
             </SafeAreaView>
           )}
-          <View style={{ position: 'absolute', bottom: 20, right: 20, backgroundColor: '#000', borderRadius: 50, padding: 10 }}>
+          <View style={{ position: 'absolute', bottom: 20, right: 20, backgroundColor: 'white', borderRadius: 50, padding: 10 }}>
             <TouchableOpacity onPress={() => setOpenList(!openList)}>
-              {openList ? <Icon name='map' size={24} color='white'/> : <Icon name='list' size={24} color='white'/> }
+              {openList ? <Icon name='map' size={24} color='black'/> : <Icon name='list' size={24} color='black'/> }
             </TouchableOpacity>
           </View>
         </View>
+                    
         <Modal
           animationType="slide"
           transparent={true}
@@ -220,47 +230,45 @@ export default function MapScreen() {
           }}
         >
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}>
-            <View style={{ backgroundColor: 'white', width: '90%', borderRadius: 10, padding: 10 }}>
+            <View style={{ backgroundColor: 'white', width: '90%', borderRadius: 20, padding: 20, borderWidth: 2 }}>
                 <View style={{ }}>
                   <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>Filtros</Text>
                   <TouchableOpacity onPress={() => {setFilterModalVisible(false)}} style={{ position: 'absolute', top: 10, right: 10 }}>
                     <Icon name='close' size={24} color='black' />
                   </TouchableOpacity>
                 </View>
-                <Text style={{ fontWeight: 'bold', fontSize: 16, marginBottom: 10 }}>Distancia máxima</Text>
+                <Text style={{ fontWeight: "400", fontSize: 13, marginBottom: 10, marginTop: 12 }}>Distancia máxima</Text>
                 <TouchableOpacity style={{ width: '100%', height: 40, borderRadius: 5, borderWidth: 1, borderColor: 'grey', padding:10, marginBottom: 8, alignContent: 'center', justifyContent: 'center' }} >
                   <ModalDropdown
                     options={distanceOptions.map((option) => option.label)}
                     onSelect={(index, value) => setFilterValues({ ...filterValues, distancia: distanceOptions[index].value })}
                     defaultValue={filterValues.distancia ? distanceOptions.find((option) => option.value == filterValues.distancia)?.label : distanceOptions[0].label}
-                    dropdownStyle={{ width: '100%', height: 200 }}
-                    textStyle={{ fontSize: 16 }}
-                    dropdownTextStyle={{ fontSize: 16 }}
+                    dropdownStyle={{ width: '72%', height: 200 }}
+                    textStyle={{ fontSize: 13, color: filterValues.distancia == 0 ? '#B8B8B8' : 'black' }}
+                    dropdownTextStyle={{ fontSize: 13, color: 'black' }}
                   />
                 </TouchableOpacity>
-                <Text style={{ fontWeight: 'bold', fontSize: 16, marginBottom: 10 }}>Tipo</Text>
+                <Text style={{ fontWeight: "400", fontSize: 13, marginBottom: 10, marginTop: 12 }}>Tipo</Text>
                 <TouchableOpacity style={{ width: '100%', height: 40, borderRadius: 5, borderWidth: 1, borderColor: 'grey', padding:10, marginBottom: 8, alignContent: 'center', justifyContent: 'center' }}>
                   <ModalDropdown
                     options={storeTypes.map((option) => option.label)}
                     onSelect={(index, value) => setFilterValues({ ...filterValues, tipo: storeTypes[index].value })}
                     defaultValue={filterValues.tipo ? storeTypes.find((option) => option.value == filterValues.tipo)?.label : storeTypes[0].label}
-                    dropdownStyle={{ width: '100%', height: 200 }}
-                    textStyle={{ fontSize: 16 }}
-                    dropdownTextStyle={{ fontSize: 16 }}
+                    dropdownStyle={{ width: '72%', height: 200 }}
+                    textStyle={{ fontSize: 13, color: filterValues.tipo == 0 ? '#B8B8B8' : 'black' }}
+                    dropdownTextStyle={{ fontSize: 13, color: 'black' }}
                   />
                 </TouchableOpacity>
-                <Text style={{ fontWeight: 'bold', fontSize: 16, marginBottom: 10 }}>Valoración mínima</Text>
-                <ValoracionEstrellas onChangeRating={(value) => setFilterValues({ ...filterValues, puntuacion: value })} value={filterValues.puntuacion} />
-                <TouchableOpacity onPress={() => setFilterValues({
-                  distancia: 0,
-                  tipo: 0,
-                  puntuacion: 0,
-                })} style={{ width: '100%', height: 40, borderRadius: 5, justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
-                  <Text style={{ fontWeight: 'bold' }}>Limpiar filtros</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => console.log('filters:', filterValues) || filterMarkers()} style={{ backgroundColor: 'blue', width: '100%', height: 40, borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}>
-                  <Text style={{ color: 'white', fontWeight: 'bold' }}>Aplicar</Text>
-                </TouchableOpacity>
+                <Text style={{ fontWeight: "400", fontSize: 13, marginBottom: 10, marginTop: 16 }}>Valoración mínima</Text>
+                  <ValoracionEstrellas onChangeRating={(value) => setFilterValues({ ...filterValues, puntuacion: value })} value={filterValues.puntuacion} />
+                <View style={{ display: 'flex', flexDirection: 'row', marginTop: 20, width: 150 }}>
+                  <TouchableOpacity onPress={() => filterMarkers()} style={{ backgroundColor: '#888DC7', width: '100%', height: 40, borderRadius: 10, display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 10 }}>
+                    <Text style={{ color: 'white', fontWeight: 'bold' }}>Aplicar</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => setFilterValues({distancia: 0, tipo: 0, puntuacion: 0})} style={{ width: '100%', height: 40, display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 10 }}>
+                    <Text style={{ fontWeight: '400', fontSize: 13, textDecorationLine: 'underline' }}>Limpiar filtros</Text>
+                  </TouchableOpacity>
+                </View>
               </View>
           </View>
         </Modal>
