9256acf689f3bba04899eb8905cf323660809996
Estela Martínez Lozano <emarloz@upv.edu.es>
2023-12-20 15:07:40 +0100

    fix anuncios y reseñas sin foto

--- src/Servicies/AnucioService/AnucioService.tsx
+++ src/Servicies/AnucioService/AnucioService.tsx
@@ -2,54 +2,60 @@ import axios from 'axios';
 import { UploadImageBucket } from '../ImagenesService';
 
 export async function SubirAnuncio(comercio: number, fecha: Date, titulo: string, descripcion: string, imagenes: [string, string][], tipo: string) {
-  try {
-    var nombreImagenesString = comercio + "" + fecha.getDate() + fecha.getTime() + (imagenes.length-1);
-    for (let i = 0; i < imagenes.length; i++) {
-      await UploadImageBucket('Anuncios', imagenes[i][1], comercio + "" + fecha.getDate() + fecha.getTime() + i);
+    try {
+        var nombreImagenesString = null;
+        if (imagenes.length > 0) {
+            nombreImagenesString = comercio + "" + fecha.getDate() + fecha.getTime() + (imagenes.length - 1);
+        for (let i = 0; i < imagenes.length; i++) {
+            await UploadImageBucket('Anuncios', imagenes[i][1], comercio + "" + fecha.getDate() + fecha.getTime() + i);
+        }
+        }
+
+        await axios.post('https://propapi-ap58.onrender.com/api/Anuncio', {
+            IdComercio: comercio,
+            Fecha: fecha.toISOString(),
+            Titulo: titulo,
+            Descripcion: descripcion,
+            imagenes: nombreImagenesString,
+            Tipo: tipo,
+        });
+
+    } catch (error) {
+        console.error("Error al subir el anuncio", error);
     }
-
-    await axios.post('https://propapi-ap58.onrender.com/api/Anuncio', {
-      IdComercio: comercio,
-      Fecha: fecha.toISOString(),
-      Titulo: titulo,
-      Descripcion: descripcion,
-      imagenes: nombreImagenesString.trim(),
-      Tipo: tipo,
-    });
-
-  } catch (error) {
-    console.error("Error al subir el anuncio", error);
-  }
 }
 
 
 export async function SubirOferta(comercio: number, fecha: Date, titulo: string, descripcion: string, imagenes: [string, string][], tipo: string, fechaIni: Date, fechaFin: Date) {
     try {
-      var nombreImagenesString = comercio + "" + fecha.getDate() + fecha.getTime() + (imagenes.length-1);
-      for (let i = 0; i < imagenes.length; i++) {
-        await UploadImageBucket('Anuncios', imagenes[i][1], comercio + "" + fecha.getDate() + fecha.getTime() + i);
-      }
-  
-      await axios.post('https://propapi-ap58.onrender.com/api/Anuncio', {
-        idcomercio: comercio,
-        fecha: fecha.toISOString(),
-        titulo: titulo,
-        descripcion: descripcion,
-        imagenes: nombreImagenesString.trim(),
-        tipo: "oferta",
-        fechaIni: fechaIni,
-        fechaFin: fechaFin,
+        var nombreImagenesString = null;
+        if (imagenes.length > 0) {
+            nombreImagenesString = comercio + "" + fecha.getDate() + fecha.getTime() + (imagenes.length - 1)
+            for (let i = 0; i < imagenes.length; i++) {
+                await UploadImageBucket('Anuncios', imagenes[i][1], comercio + "" + fecha.getDate() + fecha.getTime() + i);
+            }
+        }
 
-      });
-  
+        await axios.post('https://propapi-ap58.onrender.com/api/Anuncio', {
+            idcomercio: comercio,
+            fecha: fecha.toISOString(),
+            titulo: titulo,
+            descripcion: descripcion,
+            imagenes: nombreImagenesString,
+            tipo: "oferta",
+            fechaIni: fechaIni,
+            fechaFin: fechaFin,
+
+        });
+
     } catch (error) {
-      console.error("Error al subir el anuncio", error);
+        console.error("Error al subir el anuncio", error);
     }
-  }
+}
 
-export async function GetAnuncioById (id : Number) {
+export async function GetAnuncioById(id: Number) {
     try {
-        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/anuncio/'+id).then((res) => {
+        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/anuncio/' + id).then((res) => {
             return res.data.$values;
         })
     } catch (error) {
@@ -57,9 +63,9 @@ export async function GetAnuncioById (id : Number) {
     }
 }
 
-export async function GetNovedadFromComercio (id : Number) {
+export async function GetNovedadFromComercio(id: Number) {
     try {
-        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/novedad/'+id).then((res) => {
+        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/novedad/' + id).then((res) => {
             return res.data.$values;
         });
     } catch (error) {
@@ -67,9 +73,9 @@ export async function GetNovedadFromComercio (id : Number) {
     }
 }
 
-export async function GetOfertasFromComercio (id : Number) {
+export async function GetOfertasFromComercio(id: Number) {
     try {
-        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/oferta/'+id).then((res) => {
+        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/oferta/' + id).then((res) => {
             return res.data.$values;
         });
     } catch (error) {
@@ -77,9 +83,9 @@ export async function GetOfertasFromComercio (id : Number) {
     }
 }
 
-export async function GetLastOfertaFromComercio (id : Number) {
+export async function GetLastOfertaFromComercio(id: Number) {
     try {
-        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/oferta/ultima/'+id).then((res) => {
+        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/oferta/ultima/' + id).then((res) => {
             return res.data.$values;
         });
     } catch (error) {
@@ -87,9 +93,9 @@ export async function GetLastOfertaFromComercio (id : Number) {
     }
 }
 
-export async function GetLastNovedadFromComercio (id : Number) {
+export async function GetLastNovedadFromComercio(id: Number) {
     try {
-        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/novedad/ultima/'+id).then((res) => {
+        return await axios.get('https://propapi-ap58.onrender.com/api/Anuncio/novedad/ultima/' + id).then((res) => {
             return res.data;
         });
     } catch (error) {
